a02dd684a9a52f38b8e65d0dcbef746c234f435a
elliottomlinson <elliottomlinson@gmail.com>
2021-03-23 20:45:19 -0400

    swapping to jekyll theme

--- docs/js/etmc.js
+++ docs/js/etmc.js
@@ -7,3 +7,19 @@ function navmenu() {
     x.className = "navbar";
   }
 }
+
+//clipboard
+function myFunction() {
+  /* Get the text field */
+  var copyText = document.getElementById("myInput");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied: " + copyText.value);
+}
